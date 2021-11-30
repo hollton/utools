c3bfbbb9a381f554aa08db4fe42bb4508d926fcc
Hollton <holltonliu@163.com>
2021-11-30 11:49:27 +0800

    add npm start

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,41 @@
-module.exports = {
-  entry: './src/index.js',
+const path = require('path')
+const HtmlwebpackPlugin = require('html-webpack-plugin')
+const { HotModuleReplacementPlugin } = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const config = {
+  entry: [
+    './src/index.js',
+  ],
   output: {
-    filename: 'dist/es-utools.min.js',
+    path: path.join(__dirname, 'dist'),
+    filename: 'es-utools.min.js',
     library: 'utools',
     libraryTarget: "umd"
   },
   module: {
-  	loaders: [{
-  		test: /\.js$/,
-  		exclude: /node_modules/,
-  		loader: 'babel-loader'
-  	}]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlwebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+      inject: 'head',
+      scriptLoading: 'blocking'
+    }),
+    new HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    open: true,
+    port: 8888,
+    inline: true,
+    hot: true
   }
 };
+module.exports = config;
